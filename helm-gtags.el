22fba7bb9b10d1bdcd0bbb683b4e1f3e5de42246
Syohei YOSHIDA <syohex@gmail.com>
2012-07-16 16:46:03 +0900

    Add 'Package-Requires' header

--- helm-gtags.el
+++ helm-gtags.el
@@ -4,6 +4,8 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-gtags
+;; Version: 0.1
+;; Package-Requires: ((helm "20120710"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
