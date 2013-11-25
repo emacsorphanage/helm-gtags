c870200b61b875004abb99fdbbbcde7e5b9433a6
Syohei YOSHIDA <syohex@gmail.com>
2013-11-26 00:06:38 +0900

    Fix required helm version

--- helm-gtags.el
+++ helm-gtags.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-gtags
 ;; Version: 1.0.7
-;; Package-Requires: ((helm "1.0"))
+;; Package-Requires: ((helm "1.5.6"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
