7d1c770228271b58ce33cf7f1802aaf1c46e4c1f
Liu Binsheng <liubinsheng@gmail.com>
2013-12-03 00:30:33 +0800

    Recenter after jumping to line. Emacs doesn't recenter automatically if smooth scrolling is configured.

--- helm-gtags.el
+++ helm-gtags.el
@@ -432,6 +432,7 @@ Always update if value of this variable is nil."
   (funcall open-func file helm-gtags-read-only)
   (goto-char (point-min))
   (forward-line (1- line))
+  (recenter)
   (helm-gtags--push-context helm-gtags-saved-context))
 
 (defun helm-gtags-parse-file-action (cand)
