42cc7d352ce8336adf25a860dc66c9d1b6542192
Syohei YOSHIDA <syohex@gmail.com>
2014-01-28 19:38:46 +0900

    Bump up version and update Changes

--- helm-gtags.el
+++ helm-gtags.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-gtags
-;; Version: 1.1.7
+;; Version: 1.1.8
 ;; Package-Requires: ((helm "1.5.6"))
 
 ;; This program is free software; you can redistribute it and/or modify
