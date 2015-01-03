29eee6a52236155353cbe04fce337c1926fda922
Syohei YOSHIDA <syohex@gmail.com>
2015-01-03 21:54:10 +0900

    Update Copyright year

--- helm-gtags.el
+++ helm-gtags.el
@@ -1,6 +1,6 @@
 ;;; helm-gtags.el --- GNU GLOBAL helm interface  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014 by Syohei YOSHIDA
+;; Copyright (C) 2015 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-gtags
