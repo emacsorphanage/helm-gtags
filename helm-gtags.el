68acd9a1b7e1197fd09971c54c0afc066155579e
Syohei YOSHIDA <syohex@gmail.com>
2015-11-08 11:48:36 +0900

    Bump up version and update Changes

--- helm-gtags.el
+++ helm-gtags.el
@@ -1,10 +1,10 @@
-;;; helm-gtags.el --- GNU GLOBAL helm interface  -*- lexical-binding: t; -*-
+;;; helm-gtags.el --- GNU GLOBAL helm interface -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2015 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-gtags
-;; Version: 1.4.9
+;; Version: 1.5.0
 ;; Package-Requires: ((helm "1.7.7") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
